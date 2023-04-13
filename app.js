26496e150c736bf6a0c12c9f0433d9f6b19873c4
akmalmaulfi <akmalmaulfi@gmail.com>
2023-04-13 12:53:14 +0700

    add testing

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express")
 const app = express()
 const router = require("./routers")
 const PORT = 3000
+const env = process.env.NODE_ENV
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(router)
 
-app.listen(PORT, () => {
-  console.log("Running on PORT ", PORT)
-})
+if (env !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Running on PORT ${PORT}`)
+  })
+}
+
+module.exports = app
